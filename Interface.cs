8a714f38d3cf6b5cce5cc7c6bd3d2bf58b172984
Reshka1 <90345948+Reshka1@users.noreply.github.com>
2021-11-29 23:10:44 +0200

    Штеукафсу

---
+++ Interface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            interface Breathe
+        {
+            void Done();
+        }
+        class BasePerson : Breathe
+        {
+            public void Done()
+            {
+                Console.WriteLine("Person is Breathe");
+            }
+        }
+        class SuperPerson : BasePerson
+        {
+            public new void Done()
+            {
+                Console.WriteLine("SuperPerson is Breathe");
+            }
+
+
+        }
+           
+         
+       
+    }
+}
