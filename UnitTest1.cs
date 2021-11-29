f83b2af4fdf287b036c72c78c6a45223a2278ea4
Reshka1 <90345948+Reshka1@users.noreply.github.com>
2021-11-29 19:13:54 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HttpClient
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HttpClient httpClient = HttpClient();
+            httpClient.Dispose();
+        }
+    }
+}
